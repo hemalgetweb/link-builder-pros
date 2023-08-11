6f7bd44d5bbc99d7a17e6c50e36d40fe2c6115d8
hmdsazzad17 <hmdsazzad17@gmail.com>
2023-08-11 18:22:28 +0600

    header updated

--- wp-content/themes/ayaa/header.php
+++ wp-content/themes/ayaa/header.php
@@ -17,30 +17,10 @@ $parallex_page = function_exists('get_field') ? get_field('parallex_page') : '';
 <html lang="en-US" class="no-js">
 
 <head>
-    <meta name="robots" content="noindex, nofollow">
-    <meta charset="UTF-8">
-    <title>Linkbuilderpros - Link Building Service Agency Website</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="misapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
-
-    <!-- Required meta tags for responsive -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Favicon and touch icons -->
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon.svg">
-
-    <!-- Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Rubik:wght@500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <!-- Website title -->
-    <title>Home</title>
-
-    <?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class('inner'); ?> data-bg-color="<?php echo esc_attr($body_background_color); ?>">
